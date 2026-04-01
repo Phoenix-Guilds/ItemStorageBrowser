033d78823b03ce4a98ce0031b0b8402f2f1e1d01
WorldOfSites.Net <126398833+WorldOfSitesNet@users.noreply.github.com>
2026-04-01 18:17:15 +0300

    Auto-update: 2026-04-01 18:17

--- ItemStorageDB.lua
+++ ItemStorageDB.lua
@@ -1,4 +1,4 @@
-ItemStorageDB_LastUpdate = 1775053845
+ItemStorageDB_LastUpdate = 1775056609
 ItemStorageDB = {
     {
         name = "Бумажная",
@@ -11486,7 +11486,7 @@ ItemStorageDB = {
     },
     {
         name = "Рец",
-        updated_at = "2026-04-01 17:30:14",
+        updated_at = "2026-04-01 18:16:20",
         money = 74134,
         empty = 0,
         location = "Оргриммар",
