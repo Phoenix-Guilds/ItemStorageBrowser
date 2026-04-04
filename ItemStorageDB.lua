a035a73b8db16b0adb2e3917065bea6ce356f1c9
WorldOfSites.Net <126398833+WorldOfSitesNet@users.noreply.github.com>
2026-04-04 21:33:49 +0300

    Auto-update: 2026-04-04 21:33

--- ItemStorageDB.lua
+++ ItemStorageDB.lua
@@ -1,4 +1,4 @@
-ItemStorageDB_LastUpdate = 1775327583
+ItemStorageDB_LastUpdate = 1775327615
 ItemStorageDB = {
     {
         name = "Бумажная",
@@ -7631,7 +7631,7 @@ ItemStorageDB = {
     },
     {
         name = "Колбасная",
-        updated_at = "2026-04-03 21:44:47",
+        updated_at = "2026-04-04 21:33:14",
         money = 13238,
         empty = 0,
         location = "Оргриммар",
@@ -7765,18 +7765,6 @@ ItemStorageDB = {
                     type = "",
                     subtype = "",
                     required_level = "",
-                    retention = "",
-                },
-            },
-            {
-                link = "|cffffffff|Hitem:12213:0:0:0:0:0:0:0:10|h[Десерт из мертвечины]|h|r",
-                name = "Десерт из мертвечины",
-                count = 7,
-                details = {
-                    quality = 1,
-                    type = "",
-                    subtype = "",
-                    required_level = "25",
                     retention = "",
                 },
             },
