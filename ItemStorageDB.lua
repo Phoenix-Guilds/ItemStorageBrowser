33024a70bdc1037a564de1e995878a5f31977534
WorldOfSites.Net <126398833+WorldOfSitesNet@users.noreply.github.com>
2026-04-09 19:39:59 +0300

    Auto-update: 2026-04-09 19:39

--- ItemStorageDB.lua
+++ ItemStorageDB.lua
@@ -1,4 +1,4 @@
-ItemStorageDB_LastUpdate = 1775750465
+ItemStorageDB_LastUpdate = 1775752775
 ItemStorageDB = {
     {
         name = "Бумажная",
@@ -18110,7 +18110,7 @@ ItemStorageDB = {
     },
     {
         name = "Сумчатая",
-        updated_at = "2026-04-09 15:53:03",
+        updated_at = "2026-04-09 19:39:08",
         money = 9245,
         empty = 15,
         location = "Оргриммар",
@@ -18202,7 +18202,7 @@ ItemStorageDB = {
             {
                 link = "|cff1eff00|Hitem:21841:0:0:0:0:0:0:0:10|h[Сумка из ткани Пустоты]|h|r",
                 name = "Сумка из ткани Пустоты",
-                count = 77,
+                count = 73,
                 details = {
                     quality = 2,
                     type = "",
