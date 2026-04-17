bd6329ad9c5a918c68f5cdc71f092694e8209844
WorldOfSites.Net <126398833+WorldOfSitesNet@users.noreply.github.com>
2026-04-18 00:00:02 +0300

    Auto-update: 2026-04-18 00:00

--- ItemStorageDB.lua
+++ ItemStorageDB.lua
@@ -1,4 +1,4 @@
-ItemStorageDB_LastUpdate = 1776459480
+ItemStorageDB_LastUpdate = 1776459572
 ItemStorageDB = {
     {
         name = "Бумажная",
@@ -5696,7 +5696,7 @@ ItemStorageDB = {
     },
     {
         name = "Каменюшка",
-        updated_at = "2026-04-17 23:47:41",
+        updated_at = "2026-04-17 23:59:09",
         money = 82572,
         empty = 15,
         location = "Оргриммар",
@@ -5738,11 +5738,11 @@ ItemStorageDB = {
                 },
             },
             {
-                link = "|cffffffff|Hitem:40772:0:0:0:0:0:0:0:10|h[Гномский армейский нож]|h|r",
-                name = "Гномский армейский нож",
+                link = "|cffa335ee|Hitem:45881:0:0:0:0:0:0:0:10|h[Искрящийся самоцвет бури]|h|r",
+                name = "Искрящийся самоцвет бури",
                 count = 1,
                 details = {
-                    quality = 1,
+                    quality = 4,
                     type = "",
                     subtype = "",
                     required_level = "",
@@ -6016,7 +6016,7 @@ ItemStorageDB = {
             {
                 link = "|cff1eff00|Hitem:7909:0:0:0:0:0:0:0:10|h[Аквамарин]|h|r",
                 name = "Аквамарин",
-                count = 193,
+                count = 199,
                 details = {
                     quality = 2,
                     type = "",
@@ -6290,90 +6290,6 @@ ItemStorageDB = {
                 },
             },
             {
-                link = "|cff1eff00|Hitem:39911:0:0:0:0:0:0:0:10|h[Рунический кровавый камень]|h|r",
-                name = "Рунический кровавый камень",
-                count = 3,
-                details = {
-                    quality = 2,
-                    type = "",
-                    subtype = "",
-                    required_level = "",
-                    retention = "",
-                },
-            },
-            {
-                link = "|cff0070dd|Hitem:41339:0:0:0:0:0:0:0:10|h[Стремительный алмаз небесного сияния]|h|r",
-                name = "Стремительный алмаз небесного сияния",
-                count = 4,
-                details = {
-                    quality = 3,
-                    type = "",
-                    subtype = "",
-                    required_level = "",
-                    retention = "",
-                },
-            },
-            {
-                link = "|cff0070dd|Hitem:40053:0:0:0:0:0:0:0:10|h[Нетронутый императорский топаз]|h|r",
-                name = "Нетронутый императорский топаз",
-                count = 2,
-                details = {
-                    quality = 3,
-                    type = "",
-                    subtype = "",
-                    required_level = "",
-                    retention = "",
-                },
-            },
-            {
-                link = "|cff0070dd|Hitem:40012:0:0:0:0:0:0:0:10|h[Сверкающее сияние осени]|h|r",
-                name = "Сверкающее сияние осени",
-                count = 2,
-                details = {
-                    quality = 3,
-                    type = "",
-                    subtype = "",
-                    required_level = "",
-                    retention = "",
-                },
-            },
-            {
-                link = "|cff0070dd|Hitem:39999:0:0:0:0:0:0:0:10|h[Яркий алый рубин]|h|r",
-                name = "Яркий алый рубин",
-                count = 1,
-                details = {
-                    quality = 3,
-                    type = "",
-                    subtype = "",
-                    required_level = "",
-                    retention = "",
-                },
-            },
-            {
-                link = "|cffa335ee|Hitem:45881:0:0:0:0:0:0:0:10|h[Искрящийся самоцвет бури]|h|r",
-                name = "Искрящийся самоцвет бури",
-                count = 1,
-                details = {
-                    quality = 4,
-                    type = "",
-                    subtype = "",
-                    required_level = "",
-                    retention = "",
-                },
-            },
-            {
-                link = "|cffffffff|Hitem:40892:0:0:0:0:0:0:0:10|h[Кирка-молот]|h|r",
-                name = "Кирка-молот",
-                count = 1,
-                details = {
-                    quality = 1,
-                    type = "",
-                    subtype = "",
-                    required_level = "",
-                    retention = "",
-                },
-            },
-            {
                 link = "|cffffffff|Hitem:3486:0:0:0:0:0:0:0:10|h[Тяжелый шлифовальный камень]|h|r",
                 name = "Тяжелый шлифовальный камень",
                 count = 138,
@@ -6436,6 +6352,66 @@ ItemStorageDB = {
             {
                 link = "|cff0070dd|Hitem:36921:0:0:0:0:0:0:0:10|h[Сияние осени]|h|r",
                 name = "Сияние осени",
+                count = 1,
+                details = {
+                    quality = 3,
+                    type = "",
+                    subtype = "",
+                    required_level = "",
+                    retention = "",
+                },
+            },
+            {
+                link = "|cff1eff00|Hitem:39911:0:0:0:0:0:0:0:10|h[Рунический кровавый камень]|h|r",
+                name = "Рунический кровавый камень",
+                count = 3,
+                details = {
+                    quality = 2,
+                    type = "",
+                    subtype = "",
+                    required_level = "",
+                    retention = "",
+                },
+            },
+            {
+                link = "|cff0070dd|Hitem:41339:0:0:0:0:0:0:0:10|h[Стремительный алмаз небесного сияния]|h|r",
+                name = "Стремительный алмаз небесного сияния",
+                count = 4,
+                details = {
+                    quality = 3,
+                    type = "",
+                    subtype = "",
+                    required_level = "",
+                    retention = "",
+                },
+            },
+            {
+                link = "|cff0070dd|Hitem:40053:0:0:0:0:0:0:0:10|h[Нетронутый императорский топаз]|h|r",
+                name = "Нетронутый императорский топаз",
+                count = 2,
+                details = {
+                    quality = 3,
+                    type = "",
+                    subtype = "",
+                    required_level = "",
+                    retention = "",
+                },
+            },
+            {
+                link = "|cff0070dd|Hitem:40012:0:0:0:0:0:0:0:10|h[Сверкающее сияние осени]|h|r",
+                name = "Сверкающее сияние осени",
+                count = 2,
+                details = {
+                    quality = 3,
+                    type = "",
+                    subtype = "",
+                    required_level = "",
+                    retention = "",
+                },
+            },
+            {
+                link = "|cff0070dd|Hitem:39999:0:0:0:0:0:0:0:10|h[Яркий алый рубин]|h|r",
+                name = "Яркий алый рубин",
                 count = 1,
                 details = {
                     quality = 3,
