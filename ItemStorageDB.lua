0bd74d7994c948757f806706616cf7eb06b2217b
WorldOfSites.Net <126398833+WorldOfSitesNet@users.noreply.github.com>
2026-04-18 16:06:54 +0300

    Auto-update: 2026-04-18 16:06

--- ItemStorageDB.lua
+++ ItemStorageDB.lua
@@ -1,4 +1,4 @@
-ItemStorageDB_LastUpdate = 1776517503
+ItemStorageDB_LastUpdate = 1776517595
 ItemStorageDB = {
     {
         name = "Бумажная",
@@ -12734,7 +12734,7 @@ ItemStorageDB = {
     },
     {
         name = "Пыляшка",
-        updated_at = "2026-04-17 23:52:44",
+        updated_at = "2026-04-18 16:06:08",
         money = 38278,
         empty = 0,
         location = "Оргриммар",
@@ -12958,7 +12958,7 @@ ItemStorageDB = {
             {
                 link = "|cffffffff|Hitem:6338:0:0:0:0:0:0:0:10|h[Серебряный жезл]|h|r",
                 name = "Серебряный жезл",
-                count = 2,
+                count = 1,
                 details = {
                     quality = 1,
                     type = "",
