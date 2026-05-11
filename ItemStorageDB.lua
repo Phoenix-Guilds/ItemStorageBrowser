53c221ca6e93533d31c2999a42c5494265f6a1e7
WorldOfSites.Net <126398833+WorldOfSitesNet@users.noreply.github.com>
2026-05-11 11:41:43 +0300

    Warehouse auto-sync: 2026-05-11 11:41:42

--- ItemStorageDB.lua
+++ ItemStorageDB.lua
@@ -1,4 +1,4 @@
-ItemStorageDB_LastUpdate = 1778488846
+ItemStorageDB_LastUpdate = 1778488902
 ItemStorageDB = {
     {
         name = "Бумажная",
@@ -10936,7 +10936,7 @@ ItemStorageDB = {
     },
     {
         name = "Шмоточка",
-        updated_at = 1778488842,
+        updated_at = 1778488899,
         money = 343936,
         empty = 10,
         location = "Оргриммар",
