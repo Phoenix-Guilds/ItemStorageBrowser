b261457270630b22e4a2a21ef834a9c041cbf534
WorldOfSites.Net <126398833+WorldOfSitesNet@users.noreply.github.com>
2026-05-20 08:48:30 +0300

    Auto-update: 2026-05-20 08:48

--- ItemStorageDB.lua
+++ ItemStorageDB.lua
@@ -1,4 +1,4 @@
-ItemStorageDB_LastUpdate = 1779256060
+ItemStorageDB_LastUpdate = 1779256016
 ItemStorageDB = {
     {
         name = "Бумажная",
@@ -8995,9 +8995,9 @@ ItemStorageDB = {
     },
     {
         name = "Тканюшка",
-        updated_at = 1779256057,
+        updated_at = 1779253600,
         money = 1153902,
-        empty = 114,
+        empty = 115,
         location = "Оргриммар",
         items = {
             {
@@ -9063,7 +9063,7 @@ ItemStorageDB = {
             {
                 link = "|cffffffff|Hitem:2592:0:0:0:0:0:0:0:10|h[Шерсть]|h|r",
                 name = "Шерсть",
-                count = 634,
+                count = 631,
             },
             {
                 link = "|cffffffff|Hitem:2996:0:0:0:0:0:0:0:10|h[Рулон льняной ткани]|h|r",
@@ -9093,7 +9093,7 @@ ItemStorageDB = {
             {
                 link = "|cffffffff|Hitem:4306:0:0:0:0:0:0:0:10|h[Шелковый материал]|h|r",
                 name = "Шелковый материал",
-                count = 756,
+                count = 743,
             },
             {
                 link = "|cffffffff|Hitem:4337:0:0:0:0:0:0:0:10|h[Плотная паутина]|h|r",
