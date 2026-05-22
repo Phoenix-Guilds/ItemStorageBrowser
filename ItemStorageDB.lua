7bc2e979a3ac51cbffd49168ce34d8da6a0e9846
WorldOfSites.Net <126398833+WorldOfSitesNet@users.noreply.github.com>
2026-05-22 14:52:27 +0300

    Warehouse auto-sync: 2026-05-22 14:52:25

--- ItemStorageDB.lua
+++ ItemStorageDB.lua
@@ -1,4 +1,4 @@
-ItemStorageDB_LastUpdate = 1779450708
+ItemStorageDB_LastUpdate = 1779450745
 ItemStorageDB = {
     {
         name = "Бумажная",
@@ -3434,9 +3434,9 @@ ItemStorageDB = {
     },
     {
         name = "Кожистая",
-        updated_at = 1779450514,
+        updated_at = 1779450743,
         money = 306084,
-        empty = 127,
+        empty = 126,
         location = "Оргриммар",
         items = {
             {
@@ -3542,12 +3542,12 @@ ItemStorageDB = {
             {
                 link = "|cffffffff|Hitem:2318:0:0:0:0:0:0:0:10|h[Тонкая кожа]|h|r",
                 name = "Тонкая кожа",
-                count = 1144,
+                count = 1157,
             },
             {
                 link = "|cffffffff|Hitem:2319:0:0:0:0:0:0:0:10|h[Жесткая кожа]|h|r",
                 name = "Жесткая кожа",
-                count = 907,
+                count = 918,
             },
             {
                 link = "|cffffffff|Hitem:2320:0:0:0:0:0:0:0:10|h[Грубая нить]|h|r",
@@ -3732,7 +3732,7 @@ ItemStorageDB = {
             {
                 link = "|cffffffff|Hitem:783:0:0:0:0:0:0:0:10|h[Легкая шкура]|h|r",
                 name = "Легкая шкура",
-                count = 305,
+                count = 307,
             },
             {
                 link = "|cffffffff|Hitem:8150:0:0:0:0:0:0:0:10|h[Глубокоскальная соль]|h|r",
