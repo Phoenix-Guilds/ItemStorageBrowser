bd1bda7f99df2f9eed619c685850594b49a3ee8a
WorldOfSites.Net <126398833+WorldOfSitesNet@users.noreply.github.com>
2026-05-24 15:48:55 +0300

    Warehouse auto-sync: 2026-05-24 15:48:55

--- ItemStorageDB.lua
+++ ItemStorageDB.lua
@@ -1,4 +1,4 @@
-ItemStorageDB_LastUpdate = 1779626884
+ItemStorageDB_LastUpdate = 1779626935
 ItemStorageDB = {
     {
         name = "Бумажная",
@@ -2861,7 +2861,7 @@ ItemStorageDB = {
     },
     {
         name = "Каменюшка",
-        updated_at = 1779535183,
+        updated_at = 1779626928,
         money = 421241,
         empty = 65,
         location = "Оргриммар",
@@ -2934,12 +2934,12 @@ ItemStorageDB = {
             {
                 link = "|cff1eff00|Hitem:1206:0:0:0:0:0:0:0:10|h[Моховой агат]|h|r",
                 name = "Моховой агат",
-                count = 193,
+                count = 194,
             },
             {
                 link = "|cff1eff00|Hitem:1210:0:0:0:0:0:0:0:10|h[Камень теней]|h|r",
                 name = "Камень теней",
-                count = 209,
+                count = 211,
             },
             {
                 link = "|cff1eff00|Hitem:12361:0:0:0:0:0:0:0:10|h[Синий сапфир]|h|r",
@@ -3074,7 +3074,7 @@ ItemStorageDB = {
             {
                 link = "|cff1eff00|Hitem:818:0:0:0:0:0:0:0:10|h[Тигровый глаз]|h|r",
                 name = "Тигровый глаз",
-                count = 132,
+                count = 133,
             },
             {
                 link = "|cffffffff|Hitem:12365:0:0:0:0:0:0:0:10|h[Массивный камень]|h|r",
