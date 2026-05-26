5ffbb1d345408e890605174d4a8ed5421f30aaa9
WorldOfSites.Net <126398833+WorldOfSitesNet@users.noreply.github.com>
2026-05-26 17:13:54 +0300

    Warehouse auto-sync: 2026-05-26 17:13:52

--- ItemStorageDB.lua
+++ ItemStorageDB.lua
@@ -1,9 +1,9 @@
-ItemStorageDB_LastUpdate = 1779804739
+ItemStorageDB_LastUpdate = 1779804832
 ItemStorageDB = {
     {
         name = "Бумажная",
         realm = "ProxyEU Sirus x5 - 3.3.5a+",
-        updated_at = "2026-05-26 17:07:20",
+        updated_at = "2026-05-26 17:13:52",
         money = 339404,
         empty = 107,
         location = "Оргриммар",
@@ -1190,7 +1190,7 @@ ItemStorageDB = {
             },
         },
         bank = {
-            last_update = 1779804432,
+            last_update = 1779804786,
             items = {
                 {
                     link = "|cffffffff|Hitem:40896:0:0:0:0:0:0:0:10|h[Символ неистового восстановления]|h|r",
@@ -2370,7 +2370,7 @@ ItemStorageDB = {
             },
         },
         bags = {
-            last_update = 1779804434,
+            last_update = 1779804826,
             items = {
                 {
                     link = "|cff1eff00|Hitem:45912:0:0:0:0:0:0:0:10|h[Книга познания символов]|h|r",
