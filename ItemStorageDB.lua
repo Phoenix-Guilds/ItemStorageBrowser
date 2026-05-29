492a75e28ff2d067f1eeae4d6fc9855b94401e18
WorldOfSites.Net <126398833+WorldOfSitesNet@users.noreply.github.com>
2026-05-29 06:57:13 +0300

    Warehouse auto-sync: 2026-05-29 06:57:10

--- ItemStorageDB.lua
+++ ItemStorageDB.lua
@@ -1,4 +1,4 @@
-ItemStorageDB_LastUpdate = 1780027006
+ItemStorageDB_LastUpdate = 1780027030
 ItemStorageDB = {
     {
         name = "Бумажная",
@@ -4038,7 +4038,7 @@ ItemStorageDB = {
     {
         name = "Запределька",
         realm = "ProxyEU Sirus x5 - 3.3.5a+",
-        updated_at = "2026-05-27 08:48:33",
+        updated_at = "2026-05-29 06:57:10",
         money = 13629,
         empty = 150,
         location = "Оргриммар",
@@ -4380,7 +4380,7 @@ ItemStorageDB = {
             },
         },
         bank = {
-            last_update = 1779860896,
+            last_update = 1780027024,
             items = {
                 {
                     link = "|cff1eff00|Hitem:10270:0:0:0:0:0:793:0:10|h[Сапоги искусной работы со знаком совы]|h|r",
@@ -4555,7 +4555,7 @@ ItemStorageDB = {
             },
         },
         bags = {
-            last_update = 1779860908,
+            last_update = 1780027026,
             items = {
                 {
                     link = "|cff1eff00|Hitem:25087:0:0:0:0:0:-36:21:10|h[Кровоточащее Око с меткой волшебника]|h|r",
