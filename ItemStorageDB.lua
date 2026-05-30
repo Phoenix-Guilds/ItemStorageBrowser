45091356be5370b68e2807fec1790503c202ba9a
WorldOfSites.Net <126398833+WorldOfSitesNet@users.noreply.github.com>
2026-05-30 10:18:44 +0300

    Warehouse auto-sync: 2026-05-30 10:18:42

--- ItemStorageDB.lua
+++ ItemStorageDB.lua
@@ -1,4 +1,4 @@
-ItemStorageDB_LastUpdate = 1780125499
+ItemStorageDB_LastUpdate = 1780125522
 ItemStorageDB = {
     {
         name = "Бумажная",
@@ -5428,7 +5428,7 @@ ItemStorageDB = {
     {
         name = "Инженерная",
         realm = "ProxyEU Sirus x5 - 3.3.5a+",
-        updated_at = "2026-05-26 15:06:05",
+        updated_at = "2026-05-30 10:18:42",
         money = 16160,
         empty = 149,
         location = "Оргриммар",
@@ -5601,11 +5601,11 @@ ItemStorageDB = {
             {
                 link = "|cffffffff|Hitem:814:0:0:0:0:0:0:0:10|h[Бурдюк с маслом]|h|r",
                 name = "Бурдюк с маслом",
-                count = 6,
+                count = 8,
             },
         },
         bank = {
-            last_update = 1779797118,
+            last_update = 1780125517,
             items = {
                 {
                     link = "|cffffffff|Hitem:10498:0:0:0:0:0:0:0:10|h[Шлицевой гироинструмент]|h|r",
@@ -5775,12 +5775,12 @@ ItemStorageDB = {
                 {
                     link = "|cffffffff|Hitem:814:0:0:0:0:0:0:0:10|h[Бурдюк с маслом]|h|r",
                     name = "Бурдюк с маслом",
-                    count = 6,
+                    count = 8,
                 },
             },
         },
         bags = {
-            last_update = 1779797120,
+            last_update = 1780125519,
             items = {
             },
         },
