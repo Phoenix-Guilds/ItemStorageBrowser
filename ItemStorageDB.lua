e319804edda0dc7254a4802e6992633f1abde01e
WorldOfSites.Net <126398833+WorldOfSitesNet@users.noreply.github.com>
2026-06-02 19:36:55 +0300

    Warehouse auto-sync: 2026-06-02 19:36:54

--- ItemStorageDB.lua
+++ ItemStorageDB.lua
@@ -1,4 +1,4 @@
-ItemStorageDB_LastUpdate = 1780417541
+ItemStorageDB_LastUpdate = 1780418214
 ItemStorageDB = {
     {
         name = "Бумажная",
@@ -17512,7 +17512,7 @@ ItemStorageDB = {
     {
         name = "Тканюшка",
         realm = "ProxyEU Sirus x5 - 3.3.5a+",
-        updated_at = "2026-06-02 09:57:09",
+        updated_at = "2026-06-02 19:36:54",
         money = 1153902,
         empty = 119,
         location = "Оргриммар",
@@ -17649,7 +17649,7 @@ ItemStorageDB = {
             },
         },
         bank = {
-            last_update = 1780383423,
+            last_update = 1780418159,
             items = {
                 {
                     link = "|cff1eff00|Hitem:21842:0:0:0:0:0:0:0:10|h[Рулон прочной ткани Пустоты]|h|r",
@@ -17679,7 +17679,7 @@ ItemStorageDB = {
                 {
                     link = "|cffffffff|Hitem:2592:0:0:0:0:0:0:0:10|h[Шерсть]|h|r",
                     name = "Шерсть",
-                    count = 619,
+                    count = 569,
                 },
                 {
                     link = "|cffffffff|Hitem:2996:0:0:0:0:0:0:0:10|h[Рулон льняной ткани]|h|r",
@@ -17714,7 +17714,7 @@ ItemStorageDB = {
             },
         },
         bags = {
-            last_update = 1780383425,
+            last_update = 1780418161,
             items = {
                 {
                     link = "|cff0070dd|Hitem:41601:0:0:0:0:0:0:0:10|h[Сияющая чародейская нить]|h|r",
@@ -17750,6 +17750,11 @@ ItemStorageDB = {
                     link = "|cffffffff|Hitem:2321:0:0:0:0:0:0:0:10|h[Тонкая нить]|h|r",
                     name = "Тонкая нить",
                     count = 37,
+                },
+                {
+                    link = "|cffffffff|Hitem:2592:0:0:0:0:0:0:0:10|h[Шерсть]|h|r",
+                    name = "Шерсть",
+                    count = 50,
                 },
                 {
                     link = "|cffffffff|Hitem:3182:0:0:0:0:0:0:0:10|h[Паутина]|h|r",
