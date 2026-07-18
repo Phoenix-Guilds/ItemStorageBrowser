b7b00f6a7b0e11e0a8fc7fa887cdd75cc53a0969
WorldOfSites.Net <126398833+WorldOfSitesNet@users.noreply.github.com>
2026-07-18 21:19:03 +0300

    Warehouse auto-sync: 2026-07-18 21:19:01

--- ItemStorageDB.lua
+++ ItemStorageDB.lua
@@ -1,4 +1,4 @@
-ItemStorageDB_LastUpdate = 1784398720
+ItemStorageDB_LastUpdate = 1784398741
 ItemStorageDB = {
     {
         name = "Бумажная",
@@ -11553,7 +11553,7 @@ ItemStorageDB = {
     {
         name = "Пуляша",
         realm = "Proxy Sirus x5 - 3.3.5a+",
-        updated_at = "2026-07-18 21:18:40",
+        updated_at = "2026-07-18 21:19:01",
         money = 13104,
         empty = 75,
         location = "",
@@ -11880,7 +11880,7 @@ ItemStorageDB = {
             },
         },
         bags = {
-            last_update = 1784398715,
+            last_update = 1784398735,
             items = {
                 {
                     link = "|cff0070dd|Hitem:11630:0:0:0:0:0:0:0:10|h[Каменноосколочная беспатронная пуля]|h|r",
