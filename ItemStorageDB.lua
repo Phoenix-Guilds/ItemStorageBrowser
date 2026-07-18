6827daea1d5e70ea87f1b5a70a85f54d0d5f2443
WorldOfSites.Net <126398833+WorldOfSitesNet@users.noreply.github.com>
2026-07-18 12:01:07 +0300

    Warehouse auto-sync: 2026-07-18 12:01:06

--- ItemStorageDB.lua
+++ ItemStorageDB.lua
@@ -1,9 +1,9 @@
-ItemStorageDB_LastUpdate = 1784365181
+ItemStorageDB_LastUpdate = 1784365266
 ItemStorageDB = {
     {
         name = "Бумажная",
         realm = "Proxy Sirus x5 - 3.3.5a+",
-        updated_at = "2026-07-17 14:06:25",
+        updated_at = "2026-07-18 12:01:06",
         money = 339404,
         empty = 109,
         location = "Оргриммар",
@@ -136,7 +136,7 @@ ItemStorageDB = {
             {
                 link = "|cffffffff|Hitem:41103:0:0:0:0:0:0:0:10|h[Символ экзорцизма]|h|r",
                 name = "Символ экзорцизма",
-                count = 12,
+                count = 11,
             },
             {
                 link = "|cffffffff|Hitem:41104:0:0:0:0:0:0:0:10|h[Символ очищения]|h|r",
@@ -1185,7 +1185,7 @@ ItemStorageDB = {
             },
         },
         bank = {
-            last_update = 1784285375,
+            last_update = 1784365250,
             items = {
                 {
                     link = "|cffffffff|Hitem:40896:0:0:0:0:0:0:0:10|h[Символ неистового восстановления]|h|r",
@@ -1310,7 +1310,7 @@ ItemStorageDB = {
                 {
                     link = "|cffffffff|Hitem:41103:0:0:0:0:0:0:0:10|h[Символ экзорцизма]|h|r",
                     name = "Символ экзорцизма",
-                    count = 12,
+                    count = 11,
                 },
                 {
                     link = "|cffffffff|Hitem:41104:0:0:0:0:0:0:0:10|h[Символ очищения]|h|r",
@@ -2350,7 +2350,7 @@ ItemStorageDB = {
             },
         },
         bags = {
-            last_update = 1784285384,
+            last_update = 1784365255,
             items = {
                 {
                     link = "|cff1eff00|Hitem:45912:0:0:0:0:0:0:0:10|h[Книга познания символов]|h|r",
