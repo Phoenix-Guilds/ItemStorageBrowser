e864d82d22cc64c10491d0a2692d3162a1c8442d
WorldOfSites.Net <126398833+WorldOfSitesNet@users.noreply.github.com>
2026-08-25 20:40:29 +0300

    Warehouse auto-sync: 2026-08-25 20:40:27

--- ItemStorageDB.lua
+++ ItemStorageDB.lua
@@ -1,4 +1,4 @@
-ItemStorageDB_LastUpdate = 1787671214
+ItemStorageDB_LastUpdate = 1787679627
 ItemStorageDB = {
     {
         name = "Бумажная",
@@ -17748,9 +17748,9 @@ ItemStorageDB = {
     {
         name = "Сумчатая",
         realm = "Proxy Sirus x5 - 3.3.5a+",
-        updated_at = "2026-08-25 18:20:14",
+        updated_at = "2026-08-25 20:40:27",
         money = 9245,
-        empty = 60,
+        empty = 64,
         location = "Оргриммар",
         items = {
             {
@@ -17801,7 +17801,7 @@ ItemStorageDB = {
             {
                 link = "|cff1eff00|Hitem:41599:0:0:0:0:0:0:0:10|h[Сумка из ледяной ткани]|h|r",
                 name = "Сумка из ледяной ткани",
-                count = 10,
+                count = 6,
             },
             {
                 link = "|cff1eff00|Hitem:4500:0:0:0:0:0:0:0:10|h[Рюкзак путника]|h|r",
@@ -17815,7 +17815,7 @@ ItemStorageDB = {
             },
         },
         bank = {
-            last_update = 1787671145,
+            last_update = 1787679578,
             items = {
                 {
                     link = "|cff0070dd|Hitem:23775:0:0:0:0:0:0:0:10|h[Титановый ящик с инструментами]|h|r",
@@ -17880,7 +17880,7 @@ ItemStorageDB = {
             },
         },
         bags = {
-            last_update = 1787671156,
+            last_update = 1787679597,
             items = {
                 {
                     link = "|cff1eff00|Hitem:21841:0:0:0:0:0:0:0:10|h[Сумка из ткани Пустоты]|h|r",
@@ -17890,7 +17890,7 @@ ItemStorageDB = {
                 {
                     link = "|cff1eff00|Hitem:41599:0:0:0:0:0:0:0:10|h[Сумка из ледяной ткани]|h|r",
                     name = "Сумка из ледяной ткани",
-                    count = 8,
+                    count = 4,
                 },
             },
         },
