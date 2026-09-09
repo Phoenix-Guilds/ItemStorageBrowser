91a9a702f2f3a9e7f1bfb0af396306afbb8b03d2
WorldOfSites.Net <126398833+WorldOfSitesNet@users.noreply.github.com>
2026-09-09 07:07:59 +0300

    Warehouse auto-sync: 2026-09-09 07:07:55

--- ItemStorageDB.lua
+++ ItemStorageDB.lua
@@ -1,9 +1,9 @@
-ItemStorageDB_LastUpdate = 1788926806
+ItemStorageDB_LastUpdate = 1788926875
 ItemStorageDB = {
     {
         name = "Бумажная",
-        realm = "ProxyEU Sirus x5 - 3.3.5a+",
-        updated_at = "2026-09-08 20:11:29",
+        realm = "Sirus x5 - 3.3.5a+",
+        updated_at = "2026-09-09 07:07:55",
         money = 339404,
         empty = 113,
         location = "Оргриммар",
@@ -46,7 +46,7 @@ ItemStorageDB = {
             {
                 link = "|cffffffff|Hitem:40903:0:0:0:0:0:0:0:10|h[Символ глубокой раны]|h|r",
                 name = "Символ глубокой раны",
-                count = 9,
+                count = 8,
             },
             {
                 link = "|cffffffff|Hitem:40906:0:0:0:0:0:0:0:10|h[Символ быстрого восстановления]|h|r",
@@ -1165,7 +1165,7 @@ ItemStorageDB = {
             },
         },
         bank = {
-            last_update = 1788887292,
+            last_update = 1788926861,
             items = {
                 {
                     link = "|cffffffff|Hitem:40896:0:0:0:0:0:0:0:10|h[Символ неистового восстановления]|h|r",
@@ -1200,7 +1200,7 @@ ItemStorageDB = {
                 {
                     link = "|cffffffff|Hitem:40903:0:0:0:0:0:0:0:10|h[Символ глубокой раны]|h|r",
                     name = "Символ глубокой раны",
-                    count = 9,
+                    count = 8,
                 },
                 {
                     link = "|cffffffff|Hitem:40906:0:0:0:0:0:0:0:10|h[Символ быстрого восстановления]|h|r",
@@ -2320,7 +2320,7 @@ ItemStorageDB = {
             },
         },
         bags = {
-            last_update = 1788887307,
+            last_update = 1788926867,
             items = {
                 {
                     link = "|cff1eff00|Hitem:45912:0:0:0:0:0:0:0:10|h[Книга познания символов]|h|r",
