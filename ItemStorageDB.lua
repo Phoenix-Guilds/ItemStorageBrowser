07aa86398d2da1ab07c3dd2cbb43d48411308d2e
WorldOfSites.Net <126398833+WorldOfSitesNet@users.noreply.github.com>
2026-09-18 15:02:33 +0300

    Warehouse auto-sync: 2026-09-18 15:02:31

--- ItemStorageDB.lua
+++ ItemStorageDB.lua
@@ -1,4 +1,4 @@
-ItemStorageDB_LastUpdate = 1789731785
+ItemStorageDB_LastUpdate = 1789732951
 ItemStorageDB = {
     {
         name = "Бумажная",
@@ -16937,10 +16937,10 @@ ItemStorageDB = {
     },
     {
         name = "Сумчатая",
-        realm = "ProxyEU Sirus x5 - 3.3.5a+",
-        updated_at = "2026-09-18 01:33:06",
+        realm = "Sirus x5 - 3.3.5a+",
+        updated_at = "2026-09-18 15:02:31",
         money = 4245,
-        empty = 61,
+        empty = 65,
         location = "Оргриммар",
         items = {
             {
@@ -16961,7 +16961,7 @@ ItemStorageDB = {
             {
                 link = "|cff1eff00|Hitem:21841:0:0:0:0:0:0:0:10|h[Сумка из ткани Пустоты]|h|r",
                 name = "Сумка из ткани Пустоты",
-                count = 95,
+                count = 91,
             },
             {
                 link = "|cff1eff00|Hitem:30745:0:0:0:0:0:0:0:10|h[Тяжелый ящик с инструментами]|h|r",
@@ -17000,7 +17000,7 @@ ItemStorageDB = {
             },
         },
         bank = {
-            last_update = 1789684318,
+            last_update = 1789732930,
             items = {
                 {
                     link = "|cff0070dd|Hitem:23775:0:0:0:0:0:0:0:10|h[Титановый ящик с инструментами]|h|r",
@@ -17060,12 +17060,12 @@ ItemStorageDB = {
             },
         },
         bags = {
-            last_update = 1789684381,
+            last_update = 1789732945,
             items = {
                 {
                     link = "|cff1eff00|Hitem:21841:0:0:0:0:0:0:0:10|h[Сумка из ткани Пустоты]|h|r",
                     name = "Сумка из ткани Пустоты",
-                    count = 48,
+                    count = 44,
                 },
                 {
                     link = "|cff1eff00|Hitem:41599:0:0:0:0:0:0:0:10|h[Сумка из ледяной ткани]|h|r",
