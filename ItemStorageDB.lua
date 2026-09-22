89393ac4ecf0f30740ea164a9f5f46a768441a34
WorldOfSites.Net <126398833+WorldOfSitesNet@users.noreply.github.com>
2026-09-23 01:48:47 +0300

    Warehouse auto-sync: 2026-09-23 01:48:44

--- ItemStorageDB.lua
+++ ItemStorageDB.lua
@@ -1,4 +1,4 @@
-ItemStorageDB_LastUpdate = 1790117078
+ItemStorageDB_LastUpdate = 1790117324
 ItemStorageDB = {
     {
         name = "Бумажная",
@@ -15713,9 +15713,9 @@ ItemStorageDB = {
     {
         name = "Стартовая",
         realm = "ProxyEU Sirus x5 - 3.3.5a+",
-        updated_at = "2026-09-23 01:43:41",
+        updated_at = "2026-09-23 01:48:44",
         money = 108852,
-        empty = 117,
+        empty = 116,
         location = "Оргриммар",
         items = {
             {
@@ -16011,6 +16011,11 @@ ItemStorageDB = {
             {
                 link = "|cff1eff00|Hitem:3302:929:0:0:0:0:0:0:10|h[Сапоги недвижной воды]|h|r",
                 name = "Сапоги недвижной воды",
+                count = 1,
+            },
+            {
+                link = "|cff1eff00|Hitem:3306:1892:0:0:0:0:0:0:10|h[Жилет недвижной воды]|h|r",
+                name = "Жилет недвижной воды",
                 count = 1,
             },
             {
@@ -16370,7 +16375,7 @@ ItemStorageDB = {
             },
         },
         bank = {
-            last_update = 1790116881,
+            last_update = 1790117314,
             items = {
                 {
                     link = "|cff1eff00|Hitem:11287:0:0:0:0:0:0:0:10|h[Малый магический жезл]|h|r",
@@ -16585,6 +16590,11 @@ ItemStorageDB = {
                 {
                     link = "|cff1eff00|Hitem:3302:929:0:0:0:0:0:0:10|h[Сапоги недвижной воды]|h|r",
                     name = "Сапоги недвижной воды",
+                    count = 1,
+                },
+                {
+                    link = "|cff1eff00|Hitem:3306:1892:0:0:0:0:0:0:10|h[Жилет недвижной воды]|h|r",
+                    name = "Жилет недвижной воды",
                     count = 1,
                 },
                 {
@@ -16810,7 +16820,7 @@ ItemStorageDB = {
             },
         },
         bags = {
-            last_update = 1790117016,
+            last_update = 1790117315,
             items = {
                 {
                     link = "|cff1eff00|Hitem:11288:0:0:0:0:0:0:0:10|h[Большой магический жезл]|h|r",
