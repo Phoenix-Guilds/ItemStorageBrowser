0cffe8bf9612673303bb492d6bf60ad631302255
WorldOfSites.Net <126398833+WorldOfSitesNet@users.noreply.github.com>
2026-09-23 22:05:41 +0300

    Warehouse auto-sync: 2026-09-23 22:05:43

--- ItemStorageDB.lua
+++ ItemStorageDB.lua
@@ -1,4 +1,4 @@
-ItemStorageDB_LastUpdate = 1790190256
+ItemStorageDB_LastUpdate = 1790190343
 ItemStorageDB = {
     {
         name = "Бумажная",
@@ -17543,7 +17543,7 @@ ItemStorageDB = {
     {
         name = "Тканюшка",
         realm = "Sirus x5 - 3.3.5a+",
-        updated_at = "2026-09-23 21:59:06",
+        updated_at = "2026-09-23 22:05:43",
         money = 602,
         empty = 94,
         location = "Оргриммар",
@@ -17635,7 +17635,7 @@ ItemStorageDB = {
             },
         },
         bank = {
-            last_update = 1790189941,
+            last_update = 1790190338,
             items = {
                 {
                     link = "|cff1eff00|Hitem:21842:0:0:0:0:0:0:0:10|h[Рулон прочной ткани Пустоты]|h|r",
@@ -17695,7 +17695,7 @@ ItemStorageDB = {
             },
         },
         bags = {
-            last_update = 1790189942,
+            last_update = 1790190338,
             items = {
                 {
                     link = "|cff0070dd|Hitem:41601:0:0:0:0:0:0:0:10|h[Сияющая чародейская нить]|h|r",
